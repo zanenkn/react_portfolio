b625a2c81b096e51cc3fafedbb57c7b159ea1ce2
zanenkn <zane.neikena@gmail.com>
2019-04-15 15:31:45 +0200

    Adds header and footer

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+    return (
+        <footer>
+            <h1>Made with React</h1>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+const Header = () => {
+    return(
+        <nav>
+            <h1>My Portfolio</h1>
+        </nav>
+    )
+}
+
+export default Header
