10dedd879c8607b0f2161de14e47d6e5b8d686d4
zanenkn <zane.neikena@gmail.com>
2019-04-15 15:12:16 +0200

    Refactors code and extracts Hello.jsx

---
+++ src/Hello.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Hello = () => {
+    return (
+        <div> Hello World</div>
+    )
+}
+
+export default Hello
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import Hello from "./Hello"
 
-const HelloWorld = () => {
-    return <h2>Hello World</h2>
+const App = () => {
+    return <Hello /> 
 };
 
-ReactDOM.render(<HelloWorld />, document.getElementById("app"))
+ReactDOM.render(<App />, document.getElementById("app"))
 
 
