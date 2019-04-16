aa5699c4c32b98b35252838b8760cad8b55e368d
zanenkn <zane.neikena@gmail.com>
2019-04-16 11:08:20 +0200

    Extracts the CSS from Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+import './css/tailwind.css'
 
 const Header = () => {
     return(
         <nav className="header">
-            <h1 className="header-title">
-                <Link className="text-white hover:text-grey no-underline" to='/'>My Portfolio</Link>
-            </h1>
-            <ul className="list-reset flex">
-                <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: 'bold'}} to='/about'>About Me</NavLink></li>
-                <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: 'bold'}} to='/projects'>My Projects</NavLink></li>
+            <Link className="header-title" to='/'>My Portfolio</Link>
+            <ul className="header-ul">
+                <li className="header-li"><NavLink className="navlinks" activeStyle={{fontWeight: 'bold'}} to='/about'>About Me</NavLink></li>
+                <li className="header-li"><NavLink className="navlinks" activeStyle={{fontWeight: 'bold'}} to='/projects'>My Projects</NavLink></li>
             </ul>
         </nav>
     )
