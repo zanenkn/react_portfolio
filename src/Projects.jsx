eb469bd62a97cec30f8621b940f0a6e64a2c0d50
zanenkn <zane.neikena@gmail.com>
2019-04-16 13:20:45 +0200

    Refactors the Projects function into a Projects class in order to set state

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,12 +1,55 @@
-import React from "react"
+import React, { Component } from "react"
 
-const Projects = () => {
-    return (
-        <div>
-            <h1 className="content-text">My Projects</h1>
-            <p className="content-text">Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-        </div>
-    )
+// const Projects = () => {
+//     return (
+//         <div>
+//             <h1 className="content-text">My Projects</h1>
+//             <p className="content-text">Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+//         </div>
+//     )
+// };
+
+class Projects extends Component {
+    constructor() {
+        super();
+        this.state={
+            projects: [
+                {
+                    "id": 1,
+                    "name": "My First Website"
+                },
+                {
+                    "id": 2,
+                    "name": "FizzBuzz"
+                }
+            ]
+        };
+    }
+
+    render() {
+        const projects = this.state.projects
+        let projectsList
+
+        if(projects.length>0) {
+            projectsList = projects.map(project => {
+                return (
+                    <div key={project.id}>
+                        <h3 className="content-text">
+                            {project.name}
+                        </h3>
+                    </div>
+                )
+            })
+        }
+        
+        return (
+            <div>
+                <h1 className="content-text">My Projects</h1>
+                {projectsList}
+            </div>
+        )
+    }
 };
+
 
 export default Projects
