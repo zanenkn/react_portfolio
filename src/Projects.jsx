ba2d8032beb588628b3f9fc8b8d14f3ec202f4e5
zanenkn <zane.neikena@gmail.com>
2019-04-16 15:16:08 +0200

    Adds ProjectCard.jsx and restyles the projectList inside the Projects.jsx

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import axios from "axios"
+import ProjectCard from "./ProjectCard"
 
 // const Projects = () => {
 //     return (
@@ -34,10 +35,8 @@ class Projects extends Component {
         if(projects.length>0) {
             projectsList = projects.map(project => {
                 return (
-                    <div key={project.id}>
-                        <h3 className="content-text">
-                            {project.name}
-                        </h3>
+                    <div key={project.id} className="min-h-900 my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                        <ProjectCard project={project} />
                     </div>
                 )
             })
