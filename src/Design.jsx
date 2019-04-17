4b3feb081dccc28462235ed2a01e0142dd85430b
zanenkn <zane.neikena@gmail.com>
2019-04-17 21:14:01 +0200

    Sets up Design component in similar way as Project component - information is pulled from json file

--- src/Design.jsx
+++ src/Design.jsx
@@ -1,11 +1,54 @@
 import React, { Component } from "react"
 import axios from "axios"
+import DesignCard from "./DesignCard"
+import './css/tailwind.css'
 
-const Design = () => {
-    return (
-        <p className="content-text">Graphic Design lorem ipsum</p>
-    )
-}
+class Design extends Component {
+    constructor() {
+        super();
+        this.state={
+            design: []
+        };
+    }
+
+    componentDidMount(){
+        axios.get('./src/data/design.json')
+        .then(response => {
+            this.setState({
+                design: response.data
+            })
+        })
+    }
+
+    render() {
+        const design = this.state.design
+        let designList
+
+        if(design.length>0) {
+            designList = design.map(project => {
+                return (
+                    <div key={project.id} className="project-card-box-wrapper">
+                        <DesignCard project={project} />
+                    </div>
+                )
+            })
+        }
+        
+        return (
+            <div className="projects-box">
+                <div className="text-wrapper">
+                    <h1 className="content-header">My Projects</h1>
+                    <p className="content-text">This is a selection of some of my projects I have been working on.</p>
+                </div>
+
+            <div className="project-list-wrapper">
+                {designList}
+            </div>
+        </div>
+
+        )
+    }
+};
 
 
 
---
+++ src/DesignCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './css/tailwind.css'
+
+const DesignCard = (props) => {
+    let project = props.project
+    return(
+        <div key={project.id} className="project-card-wrapper">
+
+            <img src={project.image} className="project-card-img" style={{ height: '150 px', objectFit: 'cover' }} />
+
+            <div className="project-card-text" style={{ minHeight: '150px' }}>
+                <div className="project-card-header">
+                    {project.name}
+                </div>
+                
+                <p className="project-card-paragraph">
+                    {project.description}
+                </p>
+            </div>
+
+            <div className="button-wrapper">
+                <button className="button">
+                    See more
+                </button>
+            </div>
+
+        </div>
+    )
+}
+
+export default DesignCard
