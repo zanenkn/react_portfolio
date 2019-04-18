64bd15c34e9d6fa950124c00bb391aa0092a4838
zanenkn <zane.neikena@gmail.com>
2019-04-18 10:38:25 +0200

    tries to make the Netlify form to work

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,24 +2,25 @@ import React from "react"
 
 const Contact = () => {
     return (
-        
-            <form className="contact" method="POST" data-netlify="true">
-                <p>
-                    <label>Your Name: <input type="text" name="name" /></label>   
-                </p>
-                <p>
-                    <label>Your Email: <input type="email" name="email" /></label>
-                </p>
-                
-                <p>
-                    <label>Message: <textarea name="message"></textarea></label>
-                </p>
-                <p>
-                    <button type="submit">Send</button>
-                </p>
+
+        <div className="content-wrapper">
+            <form name="contact" method="post">
+              <input type="hidden" name="form-name" value="contact" />
+              <p>
+                <label>Your Name: <input type="text" name="name"/></label>
+              </p>
+              <p>
+                <label>Your Email: <input type="email" name="email"/></label>
+              </p>
+              <p>
+                <label>Message: <textarea name="message"></textarea></label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
             </form>
-              
-    )    
+        </div>      
+    )                
 }
 
 export default Contact
