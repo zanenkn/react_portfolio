c282438ed3f01325d529d500199e6a968babe241
zanenkn <zane.neikena@gmail.com>
2019-04-21 00:16:43 +0200

    Styling the contact form

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -13,13 +13,18 @@ const Contact = () => {
                     <div>Thank you for your message!</div>
                 }
                 {!success &&
+                    <>
+                    <h1 className="content-header">Contact me</h1>
+
+                    <p className="content-text">Fill in this form to send me a message. I will get back to you as soon as I read it!</p>
                     
-                    <div>
-                        <input type='text' name='name' required />
-                        <input type='email' name='email' required />
-                        <textarea name='message' required />
-                        <button>Submit</button>
+                    <div className="w-1/2 mt-12">
+                        <input className="input-field-sm" type='text' placeholder="Your name" name='name' required />
+                        <input className="input-field-sm" type='email' placeholder="Your email address" name='email' required />
+                        <textarea className="input-field" placeholder="Your message" name='message' required />
+                        <button className="button">Send</button>
                     </div>
+                    </>
                 }
             </>
         )}
