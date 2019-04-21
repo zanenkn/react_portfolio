43c9b34137ef191015532b2845db6516522d461c
zanenkn <zane.neikena@gmail.com>
2019-04-21 15:15:01 +0200

    Further styling of the contact page, adds Undraw illustration for the successful commit

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,24 +1,20 @@
 import React from "react"
 import NetlifyForm from 'react-netlify-form'
+import { UndrawMailSent } from 'react-undraw-illustrations';
 
 const Contact = () => {
     return (
     <NetlifyForm name='contact'>
         {({ error, success }) => (
             <>
-                {error &&
-                    <div>Your information was not sent. Please try again later.</div>
-                }
-                {success &&
-                    <div>Thank you for your message!</div>
-                }
+                
                 {!success &&
                     <>
                     <h1 className="content-header">Contact me</h1>
 
                     <p className="content-text">Fill in this form to send me a message. I will get back to you as soon as I read it!</p>
                     
-                    <div className="w-1/2 mt-12">
+                    <div className="contact-form-wrapper">
                         <input className="input-field-sm" type='text' placeholder="Your name" name='name' required />
                         <input className="input-field-sm" type='email' placeholder="Your email address" name='email' required />
                         <textarea className="input-field" placeholder="Your message" name='message' required />
@@ -26,6 +22,20 @@ const Contact = () => {
                     </div>
                     </>
                 }
+                {error &&
+                    <div className="mt-8 text-red">Your information was not sent. Please try again later.</div>
+                }
+                {success &&
+                    <>
+                    <h1 className="content-header">Thank you for your message!</h1>
+        
+                    <UndrawMailSent
+                        primaryColor='#019ebd'
+                        height= '250px'
+                        />
+                    </>
+                }
+
             </>
         )}
     </NetlifyForm>
